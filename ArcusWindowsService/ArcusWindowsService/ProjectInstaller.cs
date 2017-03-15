601608df3bb925c3ed1ad9473909fccbe70bee92
rasika.gaikwad <rasika.gaikwad@veritas.com>
2017-03-15 12:20:20 +0530

    Made changes to Agent App code so that it runs immediately after its installation

--- ArcusWindowsService/ArcusWindowsService/ProjectInstaller.cs
+++ ArcusWindowsService/ArcusWindowsService/ProjectInstaller.cs
@@ -15,5 +15,16 @@ namespace ArcusWindowsService
         {
             InitializeComponent();
         }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            base.OnAfterInstall(savedState);
+
+            //The following code starts the services after it is installed.
+            using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController(serviceInstaller1.ServiceName))
+            {
+                serviceController.Start();
+            }
+        }
     }
 }
