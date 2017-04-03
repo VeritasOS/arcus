23028061e0762c5688f96873f9dcac1922760c52
rasika.gaikwad <rasika.gaikwad@veritas.com>
2017-04-03 20:56:54 +0530

    Removed AgentApp.zip from project

--- BEArcus.WebApp/BEArcus.WebApp/Scripts/_references.js
+++ BEArcus.WebApp/BEArcus.WebApp/Scripts/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../agent/agentapp/server/purgedata.js" />
 /// <reference path="ai.0.22.9-build00167.min.js" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.validate.min.js" />
